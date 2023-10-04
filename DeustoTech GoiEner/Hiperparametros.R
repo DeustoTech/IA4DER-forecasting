db5898c57fcf38aa3a9a8fd10fc0bdf308448499
AsierLoinaz <asier.loinaz@opendeusto.es>
2023-10-04 21:05:10 +0200

    Bucle grande procesar numero de neuronas. Mase sigue fallando

--- DeustoTech GoiEner/Hiperparametros.R
+++ DeustoTech GoiEner/Hiperparametros.R
@@ -23,6 +23,146 @@ unzip(path, exdir = tempdir) # descomprime. Tarda un poco
 csv_files <- list.files(tempdir, pattern = ".csv$", recursive = T, full.names = F)
 
 
+# BUCLE PARA PROCESAR EL NUMERO OPTIMO DE NEURONAS
+
+resultadosDia <- tibble(
+  Hora = numeric(),
+  Predicted = numeric(),
+  sMAPE = numeric(),
+  RMSE = numeric(),
+  MASE = numeric(),
+  nNeuronas = numeric(),
+  TipoDia = character()
+)
+
+
+
+
+horas <- 0:23
+neuronas <- c(1:4, seq(5, 20, by = 5)) 
+
+
+cl <- makeCluster(4) 
+registerDoParallel(cl)
+
+
+RedNeuronalDia <- function(csv_file) {
+  csv_actual <- fread(csv_file)
+  
+  csv_actual <- csv_actual %>%
+    mutate(timestamp = as.POSIXct(timestamp, format = "%Y-%m-%d %H:%M:%OS")) %>%
+    mutate(Dia = weekdays(timestamp)) %>%
+    mutate(TipoDia = ifelse(Dia %in% c("lunes", "martes", "miércoles", "jueves", "viernes"),
+                            "Laborable", "Finde")) %>%
+    select(-imputed)
+  
+  
+  datosLab <- csv_actual %>% filter(TipoDia == "Laborable")
+  datosFinde <- csv_actual %>% filter(TipoDia == "Finde")
+  
+  # Bucle para procesar cada hora
+  foreach(hora = horas, .packages = librerias, .combine = 'c') %dopar% {
+    # Filtrar los datos para la hora actual
+    datos_hora <- csv_actual[hour(csv_actual$timestamp) == hora, ] 
+    
+    datosHoraLab <- datos_hora %>% filter(TipoDia == "Laborable") %>% distinct()
+    datosHoraFinde <- datos_hora %>% filter(TipoDia == "Finde") %>% distinct()
+    
+    
+    # Crear un tsibble para la hora actual - Laborable
+    ts1Lab <- datosHoraLab %>%
+      mutate(timestamp = as.Date(timestamp)) %>%
+      as_tsibble(key = kWh, index = timestamp) %>%
+      arrange(timestamp) 
+    
+    # Crear un tsibble para el siguiente día - Finde
+    ts1Finde <- datosHoraFinde %>%
+      mutate(timestamp = as.Date(timestamp)) %>%
+      as_tsibble(key = kWh, index = timestamp) %>%
+      arrange(timestamp) 
+    
+    foreach(numNeurona = neuronas, .packages = librerias) %dopar% {
+      # Entrenar el modelo neuronal para días laborables
+      errorsLab <- tsCV(ts1Lab$kWh, forecastNN, h = 1, window = 5, n = numNeurona) %>% na.omit()
+      actualLab <- ts1Lab$kWh[1: length(errorsLab)]
+      predictedLab <- actualLab + errorsLab
+      
+      # Calcular métricas para días laborables
+      smapeLab <- smape(actualLab, predictedLab)
+      rmseLab <- rmse(actualLab, predictedLab)
+      
+      # Almacenar los resultados en el tibble para días laborables
+      resultadosDia <<- resultadosDia %>% add_row(
+        Hora = hora,
+        Predicted = predictedLab,
+        sMAPE = smapeLab,
+        RMSE = rmseLab,
+        # MASE = NA,
+        nNeuronas = numNeurona,
+        TipoDia = "Laborable"
+      )
+      
+      # Entrenar el modelo neuronal para días de fin de semana
+      errorsFinde <- tsCV(ts1Finde$kWh, forecastNN, h = 1, window = 3, n = numNeurona) %>% na.omit()
+      actualFinde <- ts1Finde$kWh[1: length(errorsFinde)]
+      predictedFinde <- actualFinde + errorsFinde
+      
+      # Calcular métricas para días de fin de semana
+      smapeFinde <- smape(actualFinde, predictedFinde)
+      rmseFinde <- rmse(actualFinde, predictedFinde)
+      
+      # Almacenar los resultados en el tibble para días de fin de semana
+      resultadosDia <<- resultadosDia %>% add_row(
+        Hora = hora,
+        Predicted = predictedFinde,
+        sMAPE = smapeFinde,
+        RMSE = rmseFinde,
+        # MASE = NA,
+        nNeuronas = numNeurona,
+        TipoDia = "Finde"
+      )
+    }
+  }
+}
+
+
+
+resultados <- foreach(csv_file = csv_files, .combine='c', 
+        .packages = librerias) %dopar% RedNeuronalDia(csv_file)
+
+stopCluster(cl)
+
+
+
+
+# Boxplot
+
+filtradoRMSE <- resultadosDia %>%
+  filter(!is.na({{ RMSE }}))
+
+
+# Dividir los datos en una lista de data frames por Modelo
+divididoRMSE <- split(filtradoRMSE$RMSE, filtradoRMSE$nNeuronas)
+
+# Crear un vector de colores para los boxplots
+colores <- rainbow(length(divididoRMSE))
+
+# Crear un boxplot para cada modelo
+boxplot(divididoRMSE, 
+        main = "RMSE por Numero de neuronas", 
+        ylab = "RMSE",
+        col = colores,
+        names = names(divididoRMSE),
+        names.arg = filtradoRMSE$nNeuronas,
+        las = 2)  # Etiquetas de los modelos en el eje X
+
+
+
+
+
+
+
+
 
 # Pruebas con un solo archivo
 
@@ -51,10 +191,10 @@ testSet <- xts(ultimosLab$kWh[nrow(ultimosLab)], order.by = ultimosLab$timestamp
 #train <- ultimosLab[1:5, ] %>% select(-Dia, -Hora, -TipoDia)
 #test <- ultimosLab[6, ] %>% select(-Dia, -Hora, -TipoDia)
 
-modelo <- nnetar(trainSet, size = 3)
+modelo <- nnetar(trainSet, size = 1)
 pred <- forecast(modelo, h = 1)
 smape(testSet, as.numeric(pred$mean))
-mase(testSet,pred$mean)
+mase(testSet[1],pred$mean)
 
 #modelo1 <- nnetar(as.xts(train), size = 3)
 #pred1 <- forecast(modelo1, h = 1)
@@ -90,6 +230,19 @@ resultado0lab <- tibble(
   # MASE = numeric()
 )
 
+horas <- 0:23
+neuronas <- c(1, seq(5, 50, by = 5)) 
+
+lab1Resultados <- tibble(
+  Hora = numeric(),
+  Predicted = numeric(),
+  sMAPE = numeric(),
+  RMSE = numeric(),
+  MASE = numeric(),
+  numNeuronas = numeric()
+) 
+
+
 # El mase peta
 
 forecastNN <- function(x, h, n){
@@ -97,27 +250,61 @@ forecastNN <- function(x, h, n){
   return(prediccion)
 }
 
+# Prueba con todas las horas y solo laborables
 
-foreach( numNeurona = 1:20, .packages = librerias) %dopar%{
-  
-  errors <- tsCV(lab$kWh, forecastNN, h = 1, window = 5, n = numNeurona) %>% na.omit()
-  actual <- lab$kWh[1: length(errors)]
-  
-  predicted <- actual + errors
-  
-  
-  smape <- smape(actual, predicted)
-  rmse <- rmse(actual, predicted)
-  # mase <- mase(actual, predicted)
-  
-  resultado0lab <<- resultado0lab %>% add_row(
-    nNeuronas = numNeurona,
-    sMAPE = smape,
-    RMSE = rmse,
-  )
-  
+labHoras <- ts1 %>% filter(TipoDia == "Laborable")
+
+foreach(hora = horas) %dopar%{
+
+  foreach( numNeurona = neuronas, .packages = librerias) %dopar%{
+    
+    lab <- labHoras %>% filter(Hora == hora)
+    
+    errors <- tsCV(lab$kWh, forecastNN, h = 1, window = 5, n = numNeurona) %>% na.omit()
+    actual <- lab$kWh[1: length(errors)]
+    
+    predicted <- actual + errors
+    
+    
+    smape <- smape(actual, predicted)
+    rmse <- rmse(actual, predicted)
+    # mase <- mase(actual, predicted)
+    
+    lab1Resultados <<- lab1Resultados %>% add_row(
+      Hora = hora,
+      Predicted = predicted,
+      sMAPE = smape,
+      RMSE = rmse,
+      # MASE = numeric(),
+      numNeuronas = numNeurona
+    )
+    
+  }
 }
- # IMPORTANTE, NO SE VEN LOS RESULTADOS HASTA QUE ACABE. NO SE PUEDE PARAR
+
+filtradoRMSE <- lab1Resultados %>%
+  filter(!is.na({{ RMSE }}))
+
+
+# Dividir los datos en una lista de data frames por Modelo
+divididoRMSE <- split(filtradoRMSE$RMSE, filtradoRMSE$numNeuronas)
+
+# Crear un vector de colores para los boxplots
+colores <- rainbow(length(divididoRMSE))
+
+# Crear un boxplot para cada modelo
+boxplot(divididoRMSE, 
+        main = "RMSE por Numero de neuronas", 
+        ylab = "RMSE",
+        col = colores,
+        names = names(divididoRMSE),
+        names.arg = filtradoRMSE$numNeuronas,
+        las = 2)  # Etiquetas de los modelos en el eje X
+
+
+
+
+
 
 
 # Ahora tenemos que intentar con todos los CSV (mejor usar aleatorios)
@@ -189,5 +376,19 @@ RedNeuronalLaborable <- function(csv_file) {
 
 foreach(csv_file = csv_files,
         .packages = librerias) %dopar% RedNeuronalLaborable(csv_file)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
