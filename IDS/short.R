a2c5b21350dfee40e3723dac5eb2ef048e16c947
Cruz E. Borges <cruz.borges@deusto.es>
2023-10-04 19:14:53 +0200

    mejoro el paralelismo e includyo analisis de resultados

--- IDS/short.R
+++ IDS/short.R
@@ -3,23 +3,33 @@ library(vtable)
 library(zoo)
 library(forecast)
 library(doFuture)
+library(matrixStats)
+library(PMCMRplus)
+library(rcompanion)
+library(multcompView)
 #library(progressr)
+
+
+library(PMCMRplus)
+library(rcompanion)
+library(multcompView)
+
 
 plan(multisession)
 #handlers(global = TRUE)
 
 TRAIN_LIMIT <- 0.75  ### length of the training period
 COMPLETE    <- 0.75  ### amount of 0 allowed in the dataset
-SAMPLE      <- 2000   ### number of cups to assess
-
+SAMPLE      <- 2     ### number of cups to assess
 MODELS      <- c("mean","rw","snaive","simple","lr","ann","svm","arima","ses","ens")
+
+LO          <- 2*length(MODELS)
 
 cat("cups","model","kpi","mean","sd","min","q1","q2","q3","max",     "\n",sep=",",file="kpi.csv")
 
-RESULTS <- foreach(NAME = sample(list.files(path="post_cooked/",pattern="*.csv",recursive=T),SAMPLE),
+R <- foreach(NAME = sample(list.files(path="post_cooked/",pattern="*.csv",recursive=T),SAMPLE),
         .combine = 'rbind') %dofuture% {
 
-  print(NAME)
   a <- fread(paste("post_cooked/",NAME,sep=""))
   r <- zoo(a$kWh, order.by = a$time)
 
@@ -61,29 +71,56 @@ RESULTS <- foreach(NAME = sample(list.files(path="post_cooked/",pattern="*.csv",
     ##  p["ann"]   <- as.numeric(forecast(NN,h=24)$mean)
     ##  p["svm"]   <- as.numeric(forecast(SVM,h=24))
       p["svm"]   <- p["ann"] <- p["rw"]
-      p["ens"]   <- rowMeans(p,na.rm=T)
+      p["ens"]   <- rowMedians(as.matrix(p),na.rm=T)
       
       #### VALIDATION
       aux     <- rv != 0
       for(j in MODELS)
       {
-        MAPE[i-ZZ,j] <- median(ifelse(sum(aux)!=0,abs(rv[aux]-p[aux,j])/rv[aux],NA))
+        MAPE[i-ZZ,j] <- 100*median(ifelse(sum(aux)!=0,abs(rv[aux]-p[aux,j])/rv[aux],NA))
         RMSE[i-ZZ,j] <- sqrt(median((rv-p[,j])^2))
       }
     }
   }
   
+  k<-1
+  OUT <- data.frame(cups=character(LO),model=character(LO),kpi=character(LO),
+                    mean=numeric(LO),sd=numeric(LO),min=numeric(LO),
+                    q1=numeric(LO),q2=numeric(LO),q3=numeric(LO),max=numeric(LO))
   for(j in MODELS)
   {
     cat(NAME,j,"mape",mean(MAPE[,j],na.rm=T),sd(MAPE[,j],na.rm=T),quantile(MAPE[,j],c(0,0.25,0.5,0.75,1),na.rm=T),"\n",sep=",",file="kpi.csv",append=T)
     cat(NAME,j,"rmse",mean(RMSE[,j],na.rm=T),sd(RMSE[,j],na.rm=T),quantile(RMSE[,j],c(0,0.25,0.5,0.75,1),na.rm=T),"\n",sep=",",file="kpi.csv",append=T)
-  }
 
-#  c(NAME,apply(MAPE,2,median,na.rm=T))
+    OUT[k,]   <- c(NAME,j,"mape",mean(MAPE[,j],na.rm=T),sd(MAPE[,j],na.rm=T),quantile(MAPE[,j],c(0,0.25,0.5,0.75,1),na.rm=T))
+    OUT[k+1,] <- c(NAME,j,"rmse",mean(RMSE[,j],na.rm=T),sd(RMSE[,j],na.rm=T),quantile(RMSE[,j],c(0,0.25,0.5,0.75,1),na.rm=T))
+    k<-k+2
+  }
+  return(OUT)
 }
 
-z       <- fread("kpi.csv")
-#z$model <- factor(z$model)
-#z$kpi   <- factor(z$kpi)
-z[,as.list(summary(q2)), by = model]
-boxplot(q2~model*kpi,data=z[z$kpi=="mape",],outline=F)
+#R <- fread("kpi.csv")
+R$model   <- factor(R$model)
+R$kpi     <- factor(R$kpi)
+R[, 4:10] <- sapply(R[, 4:10], as.numeric)
+setDT(R)
+R[,as.list(summary(q2)), by = "model"]
+boxplot(q2~model,data=R[R$kpi=="mape",],outline=F,ylab="MAPE (%)")
+boxplot(q2~model,data=R[R$kpi=="rmse",],outline=F,ylab="RMSE (kWh)")
+kruskalTest(q2~model,data=R[R$kpi=="mape",])
+
+multiple <- kwAllPairsNemenyiTest(q2~model,data=R[R$kpi=="mape",])
+multcompLetters(fullPTable(multiple$p.value))
+plot(multiple)
+
+
+
+#
+#COUNTRY <- AGE <- GENDER <- numeric(length(names(F[,-c(1:3)])))
+#
+#for (i in names(F[,-c(1:3)]))
+#{
+#   GENDER[i]  <- t.test(as.formula(paste(as.name(i),"ID202",sep="~")),data=F)$p.value
+#   AGE[i]     <- t.test(as.formula(paste(as.name(i),"ID137",sep="~")),data=F)$p.value
+#   COUNTRY[i] <- t.test(as.formula(paste(as.name(i),"ID300",sep="~")),data=F)$p.value
+#}
