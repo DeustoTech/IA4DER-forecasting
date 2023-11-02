61ca8c59ba6ed0143f4d652df203179db4624b35
anesanjuan <ane.sanjuan@opendeusto.es>
2023-11-02 15:30:35 +0100

    resultadosNuevos

---
+++ DeustoTech GoiEner/AnalisisResultadosNuevos.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+library(lattice)
+library(caret)
+library(fpp3)
+library(lattice)
+library(forecast)
+library(purrr)
+library(data.table)
+library(tidyverse)
+
+L <- fread("ResultadosNuevosL.csv")
+CT <- fread("ResultadosNuevosCT.csv")
+CUPS <- fread("ResultadosNuevosCUPS.csv")
+
+
