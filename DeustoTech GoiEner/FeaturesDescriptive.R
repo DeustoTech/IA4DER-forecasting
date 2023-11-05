38ffb8617ed98f79893fc9ca2c17d3e295f160db
anesanjuan <ane.sanjuan@opendeusto.es>
2023-11-05 10:23:18 +0100

    descriptive statistics de features

---
+++ DeustoTech GoiEner/FeaturesDescriptive.R
@@ -0,0 +1,21 @@
+library(foreach)
+library(doParallel)
+
+
+# añadir las librerias nuevas en este vector
+
+librerias <- c("ggplot2", "lattice", "caret", "fpp3", 
+               "lattice", "forecast", "Metrics", "fable", 
+               "data.table", "xts", "future", "fable", "foreach", "doParallel", "RSNNS", "TTR", 
+               'quantmod', 'caret', 'e1071', 'nnet', 'tools', 'doFuture') 
+
+foreach(lib = librerias) %do% {
+  library(lib, character.only = TRUE)
+}
+
+features <- read.csv("features.csv")
+
+summary(features)
+
+
+
