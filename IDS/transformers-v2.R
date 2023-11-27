7b10f1d0a680b5ffa936e1dc5047a3c4988da17f
Cruz E. Borges <cruz.borges@deusto.es>
2023-11-27 15:30:32 +0100

    odifico para generar lineas y cajas

--- IDS/transformers-v2.R
+++ IDS/transformers-v2.R
@@ -5,8 +5,10 @@ library(doFuture)
 
 plan(multisession)
 
-dir.create("post_cooked/CT",  showWarnings = F, recursive = T)
-dir.create("post_cooked/LINE",showWarnings = F, recursive = T)
+dir.create("post_cooked/CT", showWarnings = F, recursive = T)
+dir.create("post_cooked/CGP",showWarnings = F, recursive = T)
+dir.create("post_cooked/LBT",showWarnings = F, recursive = T)
+
 
 ROSETA <- fread("cups_per_lines_ct-v2.csv",header=T)
 
@@ -55,5 +57,29 @@ foreach(i = unique(ROSETA$CGP)) %dofuture% {
     )
   }
 
-  fwrite(a,file=paste("post_cooked/LINE/",i,".csv",sep=""),dateTimeAs="write.csv")
+  fwrite(a,file=paste("post_cooked/CGP/",i,".csv",sep=""),dateTimeAs="write.csv")
+}
+
+foreach(i = unique(ROSETA$LBT)) %dofuture% {
+  CUPS <- ROSETA$CUPS[ROSETA$LBT == i]
+  N_SAMPLES <- length(CUPS)
+
+  a <- data.frame(time=seq(ISOdate(2021,06,01,00,00,00),length.out=24*457,by="hour"),
+                  kWh=double(24*457),
+                  issue=double(24*457))
+
+  for (j in CUPS)
+  {
+    tryCatch({
+      z <- fread(paste("post_cooked/CUPS/",j,".csv",sep=""),data.table=F)
+      a$kWh   <- a$kWh   + z$kWh
+      a$issue <- a$issue/N_SAMPLES + z$issue/N_SAMPLES
+    },
+             warning = function(w) {},
+             error   = function(e) {print(j)},
+             finally = {}
+    )
+  }
+
+  fwrite(a,file=paste("post_cooked/LBT/",i,".csv",sep=""),dateTimeAs="write.csv")
 }
