13f579d2d9f19140e1ac7e514e4cf597400ba414
AsierLoinaz <asier.loinaz@opendeusto.es>
2024-02-24 19:35:08 +0100

    van

--- DeustoTech GoiEner/Scripts/Features/featsSOLAR.R
+++ DeustoTech GoiEner/Scripts/Features/featsSOLAR.R
@@ -217,9 +217,16 @@ for(archivo in archivos) {
   datos_totales[[id_serie]] <- tibble(ID_SERIE = id_serie) %>% bind_cols(features_total, info_serie)
 }
 
-df_sin_auto <- bind_rows(datos_sin_auto)
+df_sin_auto <- bind_rows(datos_sin_auto) 
+df_sin_auto <- df_sin_auto[, 1:246] %>% select(-ID_SERIE) %>% distinct(ID, .keep_all = T)
+
 df_con_auto <- bind_rows(datos_con_auto)
+df_con_auto <- df_con_auto[, 1:311] %>% select(-ID_SERIE) %>% distinct(ID, .keep_all = T)
+
 df_totales <- bind_rows(datos_totales)
+df_totales <- df_totales[, 1:338] %>% select(-ID_SERIE) %>% distinct(ID, .keep_all = T)
+
+
 
 fwrite(df_sin_auto, "SOLAR/features_sin_autoconsumo.csv")
 fwrite(df_con_auto, "SOLAR/features_con_autoconsumo.csv")
