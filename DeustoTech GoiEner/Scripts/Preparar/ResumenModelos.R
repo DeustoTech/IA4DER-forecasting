bea9399abedefecd49254c1df1e7ecd5ad6c1a92
AsierLoinaz <asier.loinaz@opendeusto.es>
2024-02-27 18:25:33 +0100

    resumen modelos con real

--- DeustoTech GoiEner/Scripts/Preparar/ResumenModelos.R
+++ DeustoTech GoiEner/Scripts/Preparar/ResumenModelos.R
@@ -13,7 +13,7 @@ foreach(lib = librerias) %do% {
   library(lib, character.only = TRUE)
 }
 
-CUPS <- fread("Resultados/CUPS/PredFeatsComplete.csv")
+CUPS <- fread("Resultados/CUPS/PredFeats.csv")
 CUPS$ID <- basename(CUPS$ID)
 
 
@@ -21,42 +21,46 @@ CUPS$ID <- basename(CUPS$ID)
 result_df <- CUPS %>%
   group_by(ID = CUPS$ID) %>%
   summarise(
+    
+    real_mediana = median(Real, na.rm = TRUE),
+    real_q1 = quantile(Real, 0.25, na.rm = TRUE),
+    real_q3 = quantile(Real, 0.75, na.rm = TRUE), 
 
     predMedia_mediana = median(Media_pred, na.rm = TRUE),
     predMedia_q1 = quantile(Media_pred, 0.25, na.rm = TRUE),
-    predMedia_q2 = quantile(Media_pred, 0.75, na.rm = TRUE), 
+    predMedia_q3 = quantile(Media_pred, 0.75, na.rm = TRUE), 
     
     predNaive_mediana = median(Naive_pred, na.rm = TRUE),
     predNaive_q1 = quantile(Naive_pred, 0.25, na.rm = TRUE),
-    predNaive_q2 = quantile(Naive_pred, 0.75, na.rm = TRUE), 
+    predNaive_q3 = quantile(Naive_pred, 0.75, na.rm = TRUE), 
     
     predSN_mediana = median(SNaive_pred, na.rm = TRUE),
     predSN_q1 = quantile(SNaive_pred, 0.25, na.rm = TRUE),
-    predSN_q2 = quantile(SNaive_pred, 0.75, na.rm = TRUE), 
+    predSN_q3 = quantile(SNaive_pred, 0.75, na.rm = TRUE), 
    
     predArima_mediana = median(Arima_pred, na.rm = TRUE),
     predArima_q1 = quantile(Arima_pred, 0.25, na.rm = TRUE),
-    predArima_q2 = quantile(Arima_pred, 0.75, na.rm = TRUE), 
+    predArima_q3 = quantile(Arima_pred, 0.75, na.rm = TRUE), 
     
     predETS_mediana = median(ETS_pred, na.rm = TRUE),
     predETS_q1 = quantile(ETS_pred, 0.25, na.rm = TRUE),
-    predETS_q2 = quantile(ETS_pred, 0.75, na.rm = TRUE), 
+    predETS_q3 = quantile(ETS_pred, 0.75, na.rm = TRUE), 
     
     predNN_mediana = median(NN_pred, na.rm = TRUE),
     predNN_q1 = quantile(NN_pred, 0.25, na.rm = TRUE),
-    predNN_q2 = quantile(NN_pred, 0.75, na.rm = TRUE),
+    predNN_q3 = quantile(NN_pred, 0.75, na.rm = TRUE),
     
-    predSVM_mediana = median(SVM_pred, na.rm = TRUE),
-    predSVM_q1 = quantile(SVM_pred, 0.25, na.rm = TRUE),
-    predSVM_q2 = quantile(SVM_pred, 0.75, na.rm = TRUE),
+    predSVM_mediana = median(SMV_pred, na.rm = TRUE),
+    predSVM_q1 = quantile(SMV_pred, 0.25, na.rm = TRUE),
+    predSVM_q3 = quantile(SMV_pred, 0.75, na.rm = TRUE),
     
     predEnsenmble_mediana = median(Ensenmble_pred, na.rm = TRUE),
     predEnsenmble_q1 = quantile(Ensenmble_pred, 0.25, na.rm = TRUE),
-    predEnsenmble_q2 = quantile(nsenmbleS_pred, 0.75, na.rm = TRUE),
+    predEnsenmble_q3 = quantile(Ensenmble_pred, 0.75, na.rm = TRUE),
     
     mapeMedia_mediana = median(Media_mape, na.rm = TRUE),
     mapeMedia_q1 = quantile(Media_mape, 0.25, na.rm = TRUE),
-    mapeMedia_q2 = quantile(Media_mape, 0.75, na.rm = TRUE),
+    mapeMedia_q3 = quantile(Media_mape, 0.75, na.rm = TRUE),
     
     mapeNaive_mediana = median(Naive_mape, na.rm = TRUE),
     mapeNaive_q1 = quantile(Naive_mape, 0.25, na.rm = TRUE),
@@ -78,9 +82,9 @@ result_df <- CUPS %>%
     mapeNN_q1 = quantile(NN_mape, 0.25, na.rm = TRUE),
     mapeNN_q3 = quantile(NN_mape, 0.75, na.rm = TRUE),
     
-    mapeSVM_mediana = median(SMV_pred, na.rm = TRUE),
-    mapeSVM_q1 = quantile(SMV_pred, 0.25, na.rm = TRUE),
-    mapeSVM_q3 = quantile(SMV_pred, 0.75, na.rm = TRUE),
+    mapeSVM_mediana = median(SMV_mape, na.rm = TRUE),
+    mapeSVM_q1 = quantile(SMV_mape, 0.25, na.rm = TRUE),
+    mapeSVM_q3 = quantile(SMV_mape, 0.75, na.rm = TRUE),
     
     mapeEnsemble_mediana = median(Ensenmble_mape, na.rm = TRUE),
     mapeEnsemble_q1 = quantile(Ensenmble_mape, 0.25, na.rm = TRUE),
@@ -88,7 +92,7 @@ result_df <- CUPS %>%
     
   )
 
-fwrite(result_df, file = "Resultados/CUPS/SummaryPredsFeats2.csv", col.names = T, row.names = F)
+fwrite(result_df, file = "Resultados/CUPS/SummaryPredsFeats.csv", col.names = T, row.names = F)
 
 
 
@@ -143,9 +147,13 @@ svmCT$ID <- basename(svmCT$ID)
 
 
 
-result_df <- a %>%
+result_df <- svmCUPS %>%
   group_by(ID) %>%
   summarise(
+    
+    real_mediana = median(Real, na.rm = TRUE),
+    real_q1 = quantile(Real, 0.25, na.rm = TRUE),
+    real_q3 = quantile(Real, 0.75, na.rm = TRUE), 
     
     pred_Mediana = median(Predicted, na.rm = T),
     pred_q1 = quantile(Predicted, 0.25, na.rm = TRUE),
@@ -166,7 +174,7 @@ result_df <- a %>%
     
   )
 
-
+modelo <- svmCUPS$Modelo[1]
 
 result_df <- result_df %>%
   rename_with(~paste0("pred",modelo, "_", .), starts_with("pred_")) %>%
@@ -181,7 +189,7 @@ result_df <- result_df %>%
 
 
 
-fwrite(result_df, file = "Resultados/CUPS/SummaryMedia.csv", col.names = T, row.names = F)
+fwrite(result_df, file = "Resultados/CUPS/SummaryNN.csv", col.names = T, row.names = F)
 
 
 
