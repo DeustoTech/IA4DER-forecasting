3bef1aa077e71795334b4c9c49b8291ae68f56e7
Cruz E. Borges <cruz.borges@deusto.es>
2024-02-27 10:05:04 +0100

    arreglo la cadena de if que estaba mal para procesar los solares

--- IDS/ids03_deteccion_autoconsumo_sin_vertido/data-procesing.R
+++ IDS/ids03_deteccion_autoconsumo_sin_vertido/data-procesing.R
@@ -8,6 +8,7 @@ library(zoo)
 plan(multisession)
 
 dir.create("post_cooked/SOLAR", showWarnings = F, recursive = T)
+dir.create("fig/SOLAR", showWarnings = F, recursive = T)
 
 ARROW2DF <- function(SOURCE)
 {
@@ -17,17 +18,15 @@ ARROW2DF <- function(SOURCE)
   return(as.data.frame(at))
 }
 
-VARS   <- c("FEC_LECTURA","VAL_AI","VAL_AE") # VAL_R1 VAL_R2 VAL_R3 VAL_R4
+base     <- "../data/ids03_deteccion_autoconsumo_sin_vertido/inputdata/data/"
 
-#lec  <- ARROW2DF("../data/ids03_deteccion_autoconsumo_sin_vertido/inputdata/data/anm_ids03_aut_lec_horaria_val")
-#aut  <- ARROW2DF("../data/ids03_deteccion_autoconsumo_sin_vertido/inputdata/data/anm_ids03_lec_horaria_val")
+VARS     <- c("FEC_LECTURA","VAL_AI","VAL_AE") # VAL_R1 VAL_R2 VAL_R3 VAL_R4
 
-lec    <- ARROW2DF("anm_ids03_lec_horaria_val")
-aut    <- ARROW2DF("anm_ids03_aut_lec_horaria_val")
-
-punto    <- ARROW2DF("anm_ids03_punto_suministro")
-contrato <- ARROW2DF("anm_ids03_contrato")
-poliza   <- ARROW2DF("anm_ids03_poliza")
+lec      <- ARROW2DF(paste0(base,"anm_ids03_aut_lec_horaria_val"))
+aut      <- ARROW2DF(paste0(base,"anm_ids03_lec_horaria_val"))
+punto    <- ARROW2DF(paste0(base,"anm_ids03_punto_suministro"))
+contrato <- ARROW2DF(paste0(base,"anm_ids03_contrato"))
+poliza   <- ARROW2DF(paste0(base,"anm_ids03_poliza"))
 
 # intersect(punto$COD_PS,contrato$COD_PS)
 # intersect(poliza$COD_CONTRATO,contrato$COD_CONTRATO)
@@ -35,14 +34,14 @@ poliza   <- ARROW2DF("anm_ids03_poliza")
 ROSETA <- merge(punto,contrato,by="COD_PS")
 ROSETA <- merge(ROSETA,poliza,by="COD_CONTRATO")
 
-#NAMES <- intersect(unique(aut$CUPS),unique(lec$CUPS))
+#NAME <- NAMES[1]
 NAMES <- union(unique(aut$CUPS),unique(lec$CUPS))
 B <- foreach(NAME = NAMES,
              .errorhandling = "remove",
              .combine=merge) %dofuture%
 {
-  a1 <- lec[lec$CUPS == NAME,VARS]
-  a2 <- aut[aut$CUPS == NAME,VARS]
+  a1 <- aut[aut$CUPS == NAME,VARS]
+  a2 <- lec[lec$CUPS == NAME,VARS]
 
   a1 <- zooreg(a1[,-1],order.by=unique(as.POSIXct(a1$FEC_LECTURA)))
   a2 <- zooreg(a2[,-1],order.by=unique(as.POSIXct(a2$FEC_LECTURA)))
@@ -57,23 +56,22 @@ B <- foreach(NAME = NAMES,
     if(!last(index(a1)) < first(index(a2)))
       fin <- index(a2)[1]-60*60
     aux <- c(window(a1,end=fin),a2)
-  }
-  if (length(a1) != 0)
-  {
-    a1$AUTO <- 0
-    aux     <- a1
-  }
-  if (length(a2) != 0)
-  {
-    a2$AUTO <- 1
-    aux     <- a2
+  } else { if (length(a1) != 0)
+    {
+      a1$AUTO <- 0
+      aux     <- a1
+    } else if (length(a2) != 0)
+    {
+      a2$AUTO <- 1
+      aux     <- a2
+    }
   }
 
   fwrite(data.frame(timestamp=index(aux),VAL_AI=aux$VAL_AI,
                     VAL_AE=aux$VAL_AE, AUTO=aux$AUTO),
          file=paste("post_cooked/SOLAR/",NAME,".csv",sep=""),
          dateTimeAs="write.csv",row.names=F)
-  pdf(paste("post_cooked/SOLAR/",NAME,".pdf",sep=""))
+  pdf(paste("fig/SOLAR/",NAME,".pdf",sep=""))
     plot(aux)
   dev.off()
 
