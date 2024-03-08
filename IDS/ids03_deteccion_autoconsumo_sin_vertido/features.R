b2a6c80bae399038f95148356e9283126ca68d2a
Cruz E. Borges <cruz.borges@deusto.es>
2024-03-08 18:09:33 +0100

    fichero que analiza las features del ids3

---
+++ IDS/ids03_deteccion_autoconsumo_sin_vertido/features.R
@@ -0,0 +1,31 @@
+sin <- read.csv("features_sin_autoconsumo.csv")
+con <- read.csv("features_con_autoconsumo.csv")
+
+ind <- sapply(sin, is.numeric)
+
+pdf("boxplot.pdf")
+cat("variable","avr","sd","min","Q1","Q2","Q3","max",
+    "pv pre-post","pv suministro","pv tarifa","pv cnae","\n",
+    file="p-valores.csv",sep=",")
+for (i in names(sin)[ind])
+{
+  p <- try(wilcox.test(sin[,i],con[,i],paired=T)$p.value)
+  boxplot(sin[,i],con[,i],outline=F,names=c("Sin","Con"),main=paste(i,p))
+
+  k1 <- k2 <- k3 <- NA
+  aux <- abs(con[,i]-sin[,i])
+  if (sum(is.na(aux))/length(aux) <= 0.25)
+  {
+    k1 <- try(kruskal.test(aux~sin$TIP_SUMINISTRO)$p.value)
+    k2 <- try(kruskal.test(aux~sin$COD_TARIF_IBDLA)$p.value)
+    k3 <- try(kruskal.test(aux~substring(sin$COD_CNAE,1,1))$p.value)
+
+    boxplot(aux~sin$TIP_SUMINISTRO,outline=F,main=paste(i,k1))
+    boxplot(aux~sin$COD_TARIF_IBDLA,outline=F,main=paste(i,k2))
+    boxplot(aux~substring(sin$COD_CNAE,1,1),outline=F,main=paste(i,k3))
+  }
+  cat(i,mean(aux,na.rm=T),sd(aux,na.rm=T),as.numeric(quantile(aux,c(0,0.25,0.5,0.75,1),na.rm=T)),
+      p,k1,k2,k3,"\n",file="p-valores.csv",sep=",",append=T)
+}
+dev.off()
+
