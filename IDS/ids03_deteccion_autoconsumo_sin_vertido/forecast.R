fa25e9217ca145ac3da043e7c6d73e7cefe411eb
Cruz E. Borges <cruz.borges@deusto.es>
2024-04-15 03:02:19 +0200

    hago las predicciones del caso 3

--- IDS/ids03_deteccion_autoconsumo_sin_vertido/forecast.R
+++ IDS/ids03_deteccion_autoconsumo_sin_vertido/forecast.R
@@ -1,1 +1,103 @@
- 
+#library(arrow)
+#library(iterators)
+#library(lubridate)
+
+library(data.table)
+library(stringr)
+library(doFuture)
+library(outliers)
+library(EnvStats)
+library(zoo)
+
+plan(multisession)
+
+MINDATA  <- 10
+PRINTPDF <- TRUE
+ALPHA    <- 0.01
+QMAX     <- 0.75
+PERIODS  <- c("Daily","Monthly","Weekly")
+FEATURES <- c("ENTROPY","AVG","SD","VAR","Q1","MEDIAN","Q3","TOTAL",
+              "T2.0_LLANO","T2.0_VALLE","T_SOLAR_PICO","T_SOLAR_SPICO",
+              "T2.0_PICO","T_SOLAR_SLLANO")
+
+LIM <- fread("p-valores.csv")
+
+for (k in FEATURES)
+{
+  LA  <- as.numeric(LIM[grepl(k,variable),"avr"])
+  LQ  <- as.numeric(LIM[grepl(k,variable),"Q2"])
+
+  for (p in PERIODS)
+  {
+    dir.create(paste0("fig/",k,"/",p), showWarnings = F, recursive = T)
+
+    DATA <- fread(paste0("Variation/",p,"/",p,"_",k,".csv"))
+    DATA <- DATA[,which(unlist(lapply(DATA, function(x)!all(is.na(x))))),with=FALSE]
+    DATA <- zoo(DATA)
+
+    if (PRINTPDF)
+    {
+      pdf(paste0("fig/",k,"/",p,"/boxplots.pdf"),width=100)
+        plot(DATA[,0001:0200])
+        plot(DATA[,0201:0400])
+        plot(DATA[,0401:0600])
+        plot(DATA[,0601:0800])
+        plot(DATA[,0801:1000])
+        plot(DATA[,1001:1200])
+        plot(DATA[,1201:1400])
+        plot(DATA[,1401:1600])
+        plot(DATA[,1601:1798])
+      dev.off()
+    }
+
+    B <- foreach(NAME = names(DATA),.combine = rbind,.errorhandling = "remove") %dofuture% {
+      i   <- zoo(diff(DATA[,NAME]))
+
+      if(sum(!is.na(i)) >= MINDATA)
+      {
+        if (PRINTPDF) {pdf(paste0("fig/",k,"/",p,"/",NAME,".pdf")); plot(i); dev.off()}
+
+        ECDF <- ecdf(i)
+        GT   <- grubbs.test(as.numeric(i[!is.na(i)]),type=11)
+        DT   <- dixon.test( as.numeric(i[!is.na(i)]),type=11)
+        RT   <- rosnerTest( as.numeric(i[!is.na(i)]),alpha=ALPHA,k=2,warn=FALSE)
+
+        AO   <- boxplot(i,plot=FALSE)
+        AVR  <- ifelse(length(which(i>=LA)>=1),which(i>=LA)[1],0)
+        Q2   <- ifelse(length(which(i>=LQ)>=1),which(i>=LQ)[1],0)
+        ECDFA<- ifelse(ECDF(LA)<=QMAX         ,quantile(QMAX),0)
+        ECDFQ<- ifelse(ECDF(LQ)<=QMAX         ,quantile(QMAX),0)
+        OUT  <- ifelse(length(AO$out)>=1      ,which(i %in% AO$out)[1],0)
+        GT   <- ifelse(GT$p.value<=ALPHA      ,which(i>=as.numeric(stringr::str_split_1(GT$alternative," ")[3]))[1],0)
+        DT   <- ifelse(DT$p.value<=ALPHA      ,which(i>=as.numeric(stringr::str_split_1(DT$alternative," ")[3]))[1],0)
+        RT   <- ifelse(RT$n.outlier!=0        ,RT$all.stats[RT$all.stats$Outlier == TRUE,"Obs.Num"][1],0)
+
+        data.frame(
+          CUPS  = NAME,
+          AVR   = AVR,
+          Q2    = Q2,
+          ECDFA = ECDFA,
+          ECDFQ = ECDFQ,
+          OUT   = OUT,
+          GT    = GT,
+          DT    = DT,
+          RT    = RT
+        )
+      } else {
+        data.frame(
+          CUPS  = NAME,
+          AVR   = NA,
+          Q2    = NA,
+          ECDFA = NA,
+          ECDFQ = NA,
+          OUT   = NA,
+          GT    = NA,
+          DT    = NA,
+          RT    = NA
+        )
+      }
+    }
+
+    fwrite(B,file=paste0("forecast-",p,"-",k,".csv"))
+  }
+}
