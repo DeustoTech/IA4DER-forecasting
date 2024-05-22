8016e54d032a0fa627f77b98b70cb1c1a586d0c6
AsierLoinaz <asier.loinaz@opendeusto.es>
2024-05-22 13:14:19 +0200

    CLASSIF BIEN

--- DeustoTech GoiEner/Scripts/Predicciones/ClassifSOLAR.R
+++ DeustoTech GoiEner/Scripts/Predicciones/ClassifSOLAR.R
@@ -126,27 +126,41 @@ group2 <- c("TIP_SUMINISTRO","COD_TARIF_IBDLA","TIP_EST_POLIZA","TIP_PUNTO_MEDID
 # )
 
 
-evaluar_modelo <- function(grupo_features, modelo, train, test) {
-  # set.seed(123)
-  # setDT(data)
-  # print(grupo_features)
-  train_labels <- train$hasPV
-  test_labels <- test$hasPV
-  
-  train <- train %>% select(grupo_features)
-  test <- test %>% select(grupo_features)
-  
-  # Usar directamente caret::train para evitar conflictos de nombres
-  #fit <- caret::train(x = train, y = train_labels, method = modelo, trControl = trainControl(method="cv", number=10))
-  fit <- caret::train(x = train, y = train_labels, method = modelo)
+evaluar_modelo <- function(grupo_features, modelo, train, test, i) {
 
-  pred <- predict(fit, test)
-  # accuracy <- sum(pred == test_labels) / length(test_labels)
-  accuracy <- accuracy(test_labels, pred)
-  sensi <- sensitivity(test_labels, pred)
-  speci <- specificity(test_labels, pred)
-  # mape <- mape(as.numeric(as.character(test_labels)), as.numeric(as.character(pred)))
-  return(c(accuracy, sensi, speci))
+  train_feats <- train %>% select(all_of(grupo_features))
+  train_labels <- train$hasPV
+
+  fit <- caret::train(x = train_feats, y = train_labels, method = modelo)
+  
+  # print("ENTRENADO")
+  
+  test_featsT2 <- test %>% filter(TarifCode != "96T1" & TarifCode != "97T2") %>% select(grupo_features)
+  test_featsT6 <- test %>% filter(TarifCode == "96T1" | TarifCode == "97T2") %>% select(grupo_features)
+  
+  test_labelsT2 <- test %>% filter(TarifCode != "96T1" & TarifCode != "97T2") %>% select(hasPV)
+  test_labelsT6 <- test %>% filter(TarifCode == "96T1" | TarifCode == "97T2") %>% select(hasPV)
+  
+
+  predT2 <- predict(fit, test_featsT2)
+  predT6 <- predict(fit, test_featsT6)
+  # print(length(predT2))
+  # print((test_labelsT2))
+  
+  # print(predT2)
+  accuracyT2 <- accuracy(test_labelsT2$hasPV, predT2)
+  sensiT2 <- sensitivity(test_labelsT2$hasPV, predT2)
+  speciT2 <- specificity(test_labelsT2$hasPV, predT2)
+
+  accuracyT6 <- accuracy(test_labelsT6$hasPV, predT6)
+  sensiT6 <- sensitivity(test_labelsT6$hasPV, predT6)
+  speciT6 <- specificity(test_labelsT6$hasPV, predT6)
+  # 
+  # 
+  
+  
+  return(c(accuracyT2, sensiT2, speciT2, accuracyT6, sensiT6, speciT6))
+  # return()
 }
 
 resultados <- data.frame()
@@ -178,7 +192,8 @@ fwrite(resultados, "SOLAR/resultados_clasificacion.csv", row.names = F)
 # 
 group1 <- c("ZEROS","AVG","SD","MIN","Q1","MEDIAN","Q3",
             "ENERGY","ENTROPY")
-resultadosPerms <- data.frame()
+resultadosT2 <- data.frame()
+resultadosT6 <- data.frame()
 
 
 # Normalize columns
@@ -190,6 +205,13 @@ for (col in group1){
   }
 }
 
+
+##### PONER EL DE REGRESION SI SE HACE PARA REGRESION
+c <- read.csv("SOLAR/Classif_Accuracy.csv") %>% arrange(desc(Accuracy)) %>% slice(1:20) %>% select(Grupo)
+c_list <- lapply(c$Grupo, function(x) unlist(strsplit(x, ",\\s*")))
+
+
+
 data_classif_imputed[which(is.infinite(data_classif_imputed$ENTROPY))]$ENTROPY <- 1
 
 # group1 <- c("NAs", "ZEROS")
@@ -198,22 +220,27 @@ modelos <- "rf"
 permutations <- powerSet(group1, 9)
 for(modelo in modelos) {
   
-  for (i in 2:length(permutations)){ # permutations[[1]] is empty
-  grupo <- c(permutations[[i]])
+  for (i in 1:length(c_list)){ # permutations[[1]] is empty
+    
+  # grupo <- c(permutations[[i]])
+  grupo <- c_list[[i]]
   # grupo = group1
   index_train <- createDataPartition(data_classif_imputed$hasPV, p=0.8, list=FALSE)
   train_set <- as.data.frame(data_classif_imputed[index_train, ]) %>% filter(TarifCode != "96T1" & TarifCode != "97T2")
-  test_set <- as.data.frame(data_classif_imputed[-index_train, ])
+  test_set <- as.data.frame(data_classif_imputed[-index_train, ]) 
   
-  metrics <- evaluar_modelo(grupo, modelo, train_set, test_set)
+  metrics <- evaluar_modelo(grupo, modelo, train_set, test_set, i)
   print(paste("Feature set", i, "/512 completed"))
   print(paste("Grupo 1 ", ", Modelo: ",modelo, "DONE"))
-  resultadosPerms <- rbind(resultadosPerms, c(modelo, toString(grupo), metrics[1], metrics[2], metrics[3]))
+  resultadosT2 <- rbind(resultadosT2, c(modelo, toString(grupo), metrics[1], metrics[2], metrics[3]))
+  resultadosT6 <- rbind(resultadosT6, c(modelo, toString(grupo), metrics[4], metrics[5], metrics[6]))
   }
 }
-colnames(resultadosPerms) <- c("Modelo", "Grupo", "Accuracy", "Sensitivity", "Specificity")
+colnames(resultadosT2) <- c("Modelo", "Grupo", "Accuracy", "Sensitivity", "Specificity")
+colnames(resultadosT6) <- c("Modelo", "Grupo", "Accuracy", "Sensitivity", "Specificity")
 # resultados$Grupo <- toString(group1)
-fwrite(resultadosPerms, "SOLAR/Classif_Accuracy.csv")
+fwrite(resultadosT2, "SOLAR/Classif_Accuracy_T2.csv")
+fwrite(resultadosT6, "SOLAR/Classif_Accuracy_T6.csv")
 
 # TODO ejecutar con las que todavía no hemos ejecutado (usar setdiff)
 
