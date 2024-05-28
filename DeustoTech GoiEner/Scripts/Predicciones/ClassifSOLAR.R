cea3bb0e1436bd9f5e1ee5432b64876297cbdaf5
anesanjuan <ane.sanjuan@opendeusto.es>
2024-05-28 15:08:08 +0200

    algunos cambios

--- DeustoTech GoiEner/Scripts/Predicciones/ClassifSOLAR.R
+++ DeustoTech GoiEner/Scripts/Predicciones/ClassifSOLAR.R
@@ -15,6 +15,7 @@ foreach(lib = librerias) %do% {
 }
 
 
+#cargar archivos necesarios
 feats_con_auto <- fread("SOLAR/features_con_autoconsumo_ConPV.CSV") #982 330
 feats_trampa <- fread("SOLAR/features_sin_autoconsumo_Trampa.csv") #1451  312
 hasPV_data <- fread("SOLAR/Variation/HasPV.csv") #2434    4
@@ -29,11 +30,9 @@ columns_to_select <- setdiff(names(feats_totales), names(features))
 feats_totales <- feats_totales %>% select(ID, columns_to_select)
 
 feats_totales <- merge(feats_totales, features, by = "ID")
-
 data_classif <- merge(feats_totales, hasPV_data, by = "ID") #2433  333
 
 # Quitamos todo lo de inyección, identificadores, fechas, repetidas y variables que no cambian
-
 data_classif <- data_classif %>% select(-INSTALLATION_TIMESTAMP, -FEC_BAJA_PUN_SUM,
                                         -TIP_CONTRATO, -TIP_CUALIFICACION,
                                         -FirstInjection, -InstallationDate, -DIFF_HOURS,
@@ -60,70 +59,6 @@ data_classif_imputed <- data_classif %>%
 data_classif_imputed$hasPV <- as.factor(data_classif_imputed$hasPV)
 
 #colnames(data_classif_imputed) <- gsub("-", "_", colnames(data_classif_imputed))
-
-
-
-# statistical descriptors
-group1 <- c("LENGTH","NAs","ZEROS","AVG","SD","MIN","Q1","MEDIAN","Q3",
-            "MAX","ENTROPY","IMPUTED","ENERGY")
-
-# solar periods + tariff
-# group2 <- c("P_T2.0_VALLE","P_T2.0_LLANO","P_T2.0_PICO","P_T_SOLAR_PICO","P_T_SOLAR_LLANO",
-#             "P_T_SOLAR_SPICO","P_T_SOLAR_SLLANO", "TARIF")
-
-# contracted powers an potencies
-group2 <- c("TIP_SUMINISTRO","COD_TARIF_IBDLA","TIP_EST_POLIZA","TIP_PUNTO_MEDIDA",
-            "POT_CON","POT_EST","POT_NOM", "ENERGY")
-
-# group4 <- c(
-#   "AI.week.1", "AI.week.2", "AI.week.3", "AI.week.4", "AI.week.5",
-#   "AI.week.6", "AI.week.7", "AI.week.8", "AI.week.9", "AI.week.10",
-#   "AI.week.11", "AI.week.12", "AI.week.13", "AI.week.14", "AI.week.15",
-#   "AI.week.16", "AI.week.17", "AI.week.18", "AI.week.19", "AI.week.20",
-#   "AI.week.21", "AI.week.22", "AI.week.23", "AI.week.24", "AI.week.25",
-#   "AI.week.26", "AI.week.27", "AI.week.28", "AI.week.29", "AI.week.30",
-#   "AI.week.31", "AI.week.32", "AI.week.33", "AI.week.34", "AI.week.35",
-#   "AI.week.36", "AI.week.37", "AI.week.38", "AI.week.39", "AI.week.40",
-#   "AI.week.41", "AI.week.42", "AI.week.43", "AI.week.44", "AI.week.45",
-#   "AI.week.46", "AI.week.47", "AI.week.48", "AI.week.49", "AI.week.50",
-#   "AI.week.51", "AI.week.52", "AI.week.53", "AI.month.1", "AI.month.2", "AI.month.3",
-#   "AI.month.4", "AI.month.5", "AI.month.6", "AI.month.7", "AI.month.8",
-#   "AI.month.9", "AI.month.10", "AI.month.11", "AI.month.12")
-# 
-# group5 <- c(
-#   "Total_autum_0_4", "Total_autum_5_8", "Total_autum_9_12", "Total_autum_13_16", 
-#   "Total_autum_17_20", "Total_autum_21_24", "Total_spring_0_4", "Total_spring_5_8",  
-#   "Total_spring_9_12", "Total_spring_13_16", "Total_spring_17_20", "Total_spring_21_24",
-#   "Total_summer_0_4", "Total_summer_5_8", "Total_summer_9_12", "Total_summer_13_16",
-#   "Total_summer_17_20", "Total_summer_21_24", "Total_winter_0_4", "Total_winter_5_8",  
-#   "Total_winter_9_12", "Total_winter_13_16", "Total_winter_17_20", "Total_winter_21_24",
-#   "Max_autum_0_4", "Max_autum_5_8", "Max_autum_9_12", "Max_autum_13_16",   
-#   "Max_autum_17_20", "Max_autum_21_24", "Max_spring_0_4", "Max_spring_5_8",    
-#   "Max_spring_9_12", "Max_spring_13_16", "Max_spring_17_20", "Max_spring_21_24",  
-#   "Max_summer_0_4", "Max_summer_5_8", "Max_summer_9_12", "Max_summer_13_16",  
-#   "Max_summer_17_20", "Max_summer_21_24", "Max_winter_0_4", "Max_winter_5_8",    
-#   "Max_winter_9_12", "Max_winter_13_16", "Max_winter_17_20", "Max_winter_21_24",  
-#   "Total_autum.finde", "Total_spring.finde", "Total_summer.finde", "Total_winter.finde",
-#   "Max_autum.finde", "Max_spring.finde", "Max_summer.finde", "Max_winter.finde"
-# )
-# 
-# group6 <- c(
-#   "mean.mape.week", "mean.mape.month", 
-#   "mape.AI.week.2_1", "mape.AI.week.3_2", "mape.AI.week.4_3",
-#   "mape.AI.week.5_4", "mape.AI.week.6_5", "mape.AI.week.7_6", "mape.AI.week.8_7",
-#   "mape.AI.week.9_8", "mape.AI.week.10_9", "mape.AI.week.11_10", "mape.AI.week.12_11",
-#   "mape.AI.week.13_12", "mape.AI.week.14_13", "mape.AI.week.15_14", "mape.AI.week.16_15",
-#   "mape.AI.week.17_16", "mape.AI.week.18_17", "mape.AI.week.19_18", "mape.AI.week.20_19",
-#   "mape.AI.week.21_20", "mape.AI.week.22_21", "mape.AI.week.23_22", "mape.AI.week.24_23",
-#   "mape.AI.week.25_24", "mape.AI.week.26_25", "mape.AI.week.27_26", "mape.AI.week.28_27",
-#   "mape.AI.week.29_28", "mape.AI.week.30_29", "mape.AI.week.31_30", "mape.AI.week.32_31",
-#   "mape.AI.week.33_32", "mape.AI.week.34_33", "mape.AI.week.35_34", "mape.AI.week.36_35",
-#   "mape.AI.week.37_36", "mape.AI.week.38_37", "mape.AI.week.39_38", "mape.AI.week.40_39",
-#   "mape.AI.week.41_40", "mape.AI.week.42_41", "mape.AI.week.43_42", "mape.AI.week.44_43",
-#   "mape.AI.week.45_44", "mape.AI.week.46_45", "mape.AI.week.47_46", "mape.AI.week.48_47",
-#   "mape.AI.week.49_48", "mape.AI.week.50_49", "mape.AI.week.51_50", "mape.AI.week.52_51",
-#   "mape.AI.week.53_52"
-# )
 
 
 evaluar_modelo <- function(grupo_features, modelo, train, test, i) {
--- DeustoTech GoiEner/Scripts/Predicciones/RegrSOLAR.R
+++ DeustoTech GoiEner/Scripts/Predicciones/RegrSOLAR.R
@@ -3,7 +3,6 @@ library(doParallel)
 
 
 # añadir las librerias nuevas en este vector
-
 librerias <- c("ggplot2", "lattice", "caret", "fpp3", 
                "lattice", "forecast", "Metrics", "fable", 
                "data.table", "xts", "future", "fable", "foreach", "doParallel", "RSNNS", "TTR", 
@@ -15,14 +14,16 @@ foreach(lib = librerias) %do% {
 }
 
 
-
+#cargar archivos necesarios
 feats_con_auto <- fread("SOLAR/features_con_autoconsumo_ConPV.CSV") #982 330
 feats_trampa <- fread("SOLAR/features_sin_autoconsumo_Trampa.csv") #1451  312
 hasPV_data <- fread("SOLAR/Variation/HasPV.csv") #2434    4
 features <- fread("SOLAR/features.csv") #97028    22
 
-features <- features %>% filter(ASS == "CUPS" | ASS == "SOLAR") #71021 22
+## DATA CLEANING
 
+#juntar todos los csvs
+features <- features %>% filter(ASS == "CUPS" | ASS == "SOLAR") #71021 22
 feats_totales <- rbind(feats_con_auto, feats_trampa, fill = T) #2433  330
 
 # Ejecutar estas dos lineas para seleccionar de features (cruz) solo las que no tenemos nosotros
@@ -30,18 +31,15 @@ columns_to_select <- setdiff(names(feats_totales), names(features))
 feats_totales <- feats_totales %>% select(ID, columns_to_select)
 
 feats_totales <- merge(feats_totales, features, by = "ID")
-
 data_classif <- merge(feats_totales, hasPV_data, by = "ID") #2433  333
 
-# Quitamos todo lo de inyección, identificadores, fechas, repetidas y variables que no cambian
 
+# Quitamos todo lo de inyección, identificadores, fechas, repetidas y variables que no cambian
 data_classif <- data_classif %>% select(-INSTALLATION_TIMESTAMP, -FEC_BAJA_PUN_SUM,
                                         -TIP_CONTRATO, -TIP_CUALIFICACION,
                                         -FirstInjection, -InstallationDate, -DIFF_HOURS,
                                         -COD_CNAE, -contains("AE."), -COD_PS, -COD_CLIENTE,
                                         -COD_CONTRATO, -ASS, -starts_with("FEC"), -SHARP, -ZERO, -COD_SOCIEDAD)
-
-
 
 categorical_columns <- c( "TIP_SUMINISTRO", "COD_TARIF_IBDLA", "TIP_EST_POLIZA", "CNAE", "TARIF", "SUM", "TIP_PUNTO_MEDIDA")
 
@@ -52,6 +50,7 @@ for (col in categorical_columns) {
   data_classif[[col]] <- (as.factor(data_classif[[col]]))
 }
 
+#quitar categorias que solo tienen un factor o 2
 unique_categories_to_filter <- list(
   TIP_SUMINISTRO = c(3, 4, 6, 14, 17),
   TIP_EST_POLIZA = c(1, 3), 
@@ -68,33 +67,27 @@ data_classif$TIP_EST_POLIZA <- droplevels(data_classif$TIP_EST_POLIZA)
 data_classif$CNAE <- droplevels(data_classif$CNAE)
 data_classif$SUM <- droplevels(data_classif$SUM)
 
+#renombrar algunas columnas para que funcione random forest
 for (col in colnames(data_classif)) {
-  # Reemplazar "-" por "."
   new_col_name <- gsub("-", ".", col)
-  
-  # Renombrar la columna en el dataset
   names(data_classif)[names(data_classif) == col] <- new_col_name
 }
 
+#imputar valores
 data_classif <- data_classif %>% 
   mutate(across(where(is.numeric), ~replace(., !is.finite(.), NA)))
-
 data_classif_imputed <- data_classif %>%
   mutate(across(where(is.numeric), ~if_else(is.na(.), median(., na.rm = TRUE), .)))
 
+#convertir a numerica y normalizar la variable a predecir
 data_classif_imputed$POT_AUT <- as.numeric(data_classif_imputed$POT_AUT)
-
-
 
 normalize_min_max <- function(x) {
   return((x - min(x)) / (max(x) - min(x)))
 }
 
 data_classif_imputed$POT_AUT <- normalize_min_max(data_classif_imputed$POT_AUT)
-
-
-
-data_classif_imputed <- data_classif_imputed %>% select(-ID)
+data_classif_imputed <- data_classif_imputed %>% select(-ID) #quitar el ID
 
 ensure_levels_in_train <- function(data, categorical_columns) {
   train_indices <- c()
@@ -107,77 +100,8 @@ ensure_levels_in_train <- function(data, categorical_columns) {
   return(unique(train_indices))
 }
 
-###### hasta aqui fijo si
+## PREDICCIONES
 
-
-#
-initial_train_indices <- ensure_levels_in_train(data_classif_imputed, categorical_columns)
-data_train_initial <- data_classif_imputed[initial_train_indices, ]
-remaining_data <- data_classif_imputed[-initial_train_indices, ]
-
-set.seed(123)
-remaining_train_size <- floor(0.8 * nrow(data_classif_imputed)) - nrow(data_train_initial)
-remaining_train_indices <- sample(seq_len(nrow(remaining_data)), size = remaining_train_size)
-data_train <- rbind(data_train_initial, remaining_data[remaining_train_indices, ])
-data_test <- remaining_data[-remaining_train_indices, ]
-
-for (col in categorical_columns) {
-  data_train[[col]] <- as.factor(data_train[[col]])
-  data_test[[col]] <- as.factor(data_test[[col]])
-  
-  levels(data_test[[col]]) <- levels(data_train[[col]])
-}
-
-####
-
-'''
-set.seed(123)  # Para reproducibilidad
-trainIndex <- createDataPartition(data_classif_imputed$POT_AUT, p = .8, 
-                                  list = FALSE, 
-                                  times = 1)
-data_train <- data_classif_imputed[trainIndex, ]
-data_test  <- data_classif_imputed[-trainIndex, ]
-
-
-for (col in categorical_columns) {
-  data_train[[col]] <- as.factor(data_train[[col]])
-  data_test[[col]] <- as.factor(data_test[[col]])
-  
-  levels(data_test[[col]]) <- union(levels(data_test[[col]]), levels(data_train[[col]]))
-  levels(data_train[[col]]) <- union(levels(data_train[[col]]), levels(data_test[[col]]))
-}
-
-'''
-
-
-#linear regression
-model_lm <- lm(POT_AUT ~ ., data = data_train)
-predictions_lm <- predict(model_lm, newdata = data_test)
-
-#random forest
-model_rf <- randomForest(POT_AUT ~ ., data = data_train, ntree = 100)
-predictions_rf <- predict(model_rf, newdata = data_test)
-
-#svm
-model_gbm <- caret::train(POT_AUT ~ ., data = data_train, method = 'gbm', trControl = trainControl(method = "cv", number = 10), verbose = FALSE)
-predictions_gbm <- predict(model_gbm, newdata = data_test)
-
-#CALCULATE MAPES
-mape_lm <- mape(data_test$POT_AUT, predictions_lm)
-mape_rf <- mape(data_test$POT_AUT, predictions_rf)
-mape_gbm <- mape(data_test$POT_AUT, predictions_gbm)
-
-results <- data.frame(
-  Model = c("LR", "RF", "GBM"),
-  MAPE = c(mape_lm, mape_rf, mape_gbm)
-)
-
-fwrite(results, "SOLAR/Regression_SOLAR.csv", row.names = FALSE)
-
-
-
-
-#### lo nuevo con los cambios
 group1 <- c("ZEROS","AVG","SD","MIN","Q1","MEDIAN","Q3",
             "ENERGY","ENTROPY")
 
@@ -187,7 +111,7 @@ resultadosT6 <- data.frame()
 c <- read.csv("SOLAR/resultadosPerms_regrs.csv") %>% arrange(desc(RMSE_rf)) %>% slice(1:20) %>% select(Grupo)
 c_list <- lapply(c$Grupo, function(x) unlist(strsplit(x, ",\\s*")))
 
-# Normalize columns
+# Normalizar columnas
 for (col in group1){
   if (col != "ZEROS"){
     data_classif_imputed[[col]] <- data_classif_imputed[[col]] / data_classif_imputed$MAX
@@ -200,11 +124,8 @@ data_classif_imputed[which(is.infinite(data_classif_imputed$ENTROPY))]$ENTROPY <
 
 
 evaluar_modelo <- function(grupo_features, train, test) {
-  # set.seed(123)
-  # setDT(data)
-  # print(grupo_features)
-  train_labels <- train$POT_AUT
   
+  train_labels <- train$POT_AUT
   train <- train %>% select(all_of(grupo_features), POT_AUT)
   
   #test <- test %>% filter(TarifCode != "96T1" & TarifCode != "97T2") %>% select(all_of(grupo_features), POT_AUT)
@@ -239,11 +160,10 @@ evaluar_modelo <- function(grupo_features, train, test) {
 
 permutations <- powerSet(group1, 9)
   
-  for (i in 1:length(c_list)){ # permutations[[1]] is empty
+  for (i in 1:length(c_list)){
     
     grupo <- c_list[[i]]
     print(grupo)
-    # grupo = group1
     initial_train_indices <- ensure_levels_in_train(data_classif_imputed, categorical_columns)
     data_train_initial <- data_classif_imputed[initial_train_indices, ]
     remaining_data <- data_classif_imputed[-initial_train_indices, ]
@@ -264,7 +184,6 @@ fwrite(resultadosT6, "SOLAR/resultadosPerms_regrs_t6.csv", row.names = T)
 
 datat2 <- fread("SOLAR/resultadosPerms_regrs_t2.csv")
 datat6 <- fread("SOLAR/resultadosPerms_regrs_t6.csv")
-
 
 summary(datat2)
 
