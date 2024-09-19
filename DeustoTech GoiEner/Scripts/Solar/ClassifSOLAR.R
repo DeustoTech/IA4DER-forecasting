5a4025982ef25314930efabf2ba05d98300d1dc4
AsierLoinaz <asier.loinaz@opendeusto.es>
2024-09-19 17:15:14 +0000

    clasificacion

--- DeustoTech GoiEner/Scripts/Solar/ClassifSOLAR.R
+++ DeustoTech GoiEner/Scripts/Solar/ClassifSOLAR.R
@@ -3,7 +3,7 @@ library(doParallel)
 library(doFuture)
 
 registerDoFuture()
-plan(multisession)  # Change the number of workers
+plan(multisession, workers = 30)  # Change the number of workers
 
 
 # añadir las librerias nuevas en este vector
@@ -80,6 +80,8 @@ for (col in feats){
 
 
 solar_data[which(is.infinite(solar_data$ENTROPY))]$ENTROPY <- 1
+solar_data$hasPV <- factor(solar_data$hasPV, levels = c(0, 1))
+
 
 
 cases <- c(1:4)
@@ -100,6 +102,11 @@ evaluar_modelo <- function(grupo_features, train, test) {
   model_gbm <- caret::train(hasPV ~ ., data = train, method = 'gbm', verbose = FALSE)
   predictions_gbm <- predict(model_gbm, newdata = test)
   
+  test_labels <- factor(test_labels, levels = c(0, 1))
+  predictions_lr <- factor(predictions_lr, levels = c(0, 1))
+  predictions_rf <- factor(predictions_rf, levels = c(0, 1))
+  predictions_gbm <- factor(predictions_gbm, levels = c(0, 1))
+  
   lr_accuracy <- accuracy(test_labels, predictions_lr)
   lr_sensitivity <- sensitivity(test_labels, predictions_lr)
   lr_specificity <- specificity(test_labels, predictions_lr)
@@ -115,71 +122,68 @@ evaluar_modelo <- function(grupo_features, train, test) {
 
 
 resultados <- data.frame()
-modelos <- c("rf",  "glm")
+modelos <- c("lr",  "rf", "gbm")
 
 
+# Define global variables
 permutations <- powerSet(feats, length(feats))
 howMany <- length(permutations)
-results <- data.frame() 
-globalvars <- c("evaluar_modelo", "solar_data", "permutations", "librerias", "cases", "case_progress",  "perm_progress",
-                "c1_list", "c2_list", "c3_list", "c4_list")
 
 
-# ONLY 1 time with all permutations
+# Initialize progress handlers
+handlers(global = TRUE)
+
+# Start the progress reporting
 with_progress({
-  # Initialize the outer progress for cases
-  
+  # Create a global progress bar for all cases
+  global_progress <- progressor(steps = length(cases) * (howMany - 1), message = "Processing all cases and permutations")
+  globalvars <- c("evaluar_modelo", "solar_data", "permutations", "librerias", "cases", "case_progress", "perm_progress", 
+                "c1_list", "c2_list", "c3_list", "c4_list", "howMany", "global_progress")
+  # Run the outer foreach loop for each case
   final_results <- foreach(case = cases, .combine = rbind, .options.future = list(seed = TRUE, add = TRUE, globals = globalvars, packages = librerias)) %dofuture% {
-    case_progress <- progressr::progressor(steps = length(cases), message = "Processing cases")
+    # Create a progress bar for the current case
+    case_progress <- progressr::progressor(steps = howMany - 1, message = sprintf("Processing Case %d", case))
     
-    case_progress(sprintf("Processing case %d", case))  # Update case progress
-    
+    # Initialize training and testing datasets based on the case
     train <- data.frame()
     test <- data.frame()
     
-    # Prepare training and testing data based on the case
-    if(case == 1) {
+    if (case == 1) {
       case1 <- solar_data %>% filter(TarifCode != "96T1" & TarifCode != "97T2")
-      train_idx <- createDataPartition(case1$hasPV, p=0.8, list=FALSE)
+      train_idx <- createDataPartition(case1$hasPV, p = 0.8, list = FALSE)
       train <- as.data.frame(solar_data[train_idx, ])
       test <- as.data.frame(solar_data[-train_idx, ])
-    }
-    
-    if(case == 2) {
+    } else if (case == 2) {
       case2 <- solar_data %>% filter(TarifCode == "96T1" | TarifCode == "97T2")
-      train_idx <- createDataPartition(case2$hasPV, p=0.8, list=FALSE)
+      train_idx <- createDataPartition(case2$hasPV, p = 0.8, list = FALSE)
       train <- as.data.frame(solar_data[train_idx, ])
       test <- as.data.frame(solar_data[-train_idx, ])
-    }
-    
-    if(case == 3) {
+    } else if (case == 3) {
       t2 <- solar_data %>% filter(TarifCode != "96T1" & TarifCode != "97T2")
       t6 <- solar_data %>% filter(TarifCode == "96T1" | TarifCode == "97T2")
-      train_idx <- createDataPartition(t2$hasPV, p=0.8, list=FALSE)
-      test_idx <- createDataPartition(t6$hasPV, p=0.2, list=FALSE)
+      train_idx <- createDataPartition(t2$hasPV, p = 0.8, list = FALSE)
+      test_idx <- createDataPartition(t6$hasPV, p = 0.2, list = FALSE)
+      train <- as.data.frame(solar_data[train_idx, ])
+      test <- as.data.frame(solar_data[test_idx, ])
+    } else if (case == 4) {
+      t2 <- solar_data %>% filter(TarifCode != "96T1" & TarifCode != "97T2")
+      t6 <- solar_data %>% filter(TarifCode == "96T1" | TarifCode == "97T2")
+      train_idx <- createDataPartition(t6$hasPV, p = 0.8, list = FALSE)
+      test_idx <- createDataPartition(t2$hasPV, p = 0.2, list = FALSE)
       train <- as.data.frame(solar_data[train_idx, ])
       test <- as.data.frame(solar_data[test_idx, ])
     }
     
-    if(case == 4) {
-      t2 <- solar_data %>% filter(TarifCode != "96T1" & TarifCode != "97T2")
-      t6 <- solar_data %>% filter(TarifCode == "96T1" | TarifCode == "97T2")
-      train_idx <- createDataPartition(t6$hasPV, p=0.8, list=FALSE)
-      test_idx <- createDataPartition(t2$hasPV, p=0.2, list=FALSE)
-      train <- as.data.frame(solar_data[train_idx, ])
-      test <- as.data.frame(solar_data[test_idx, ])
-    }
-    
-    # Initialize the progress for permutations
-    perm_progress <- progressr::progressor(steps = length(permutations) - 1, message = sprintf("Permutations for case %d", case))
-    
-    foreach(i = 2:length(permutations), .combine = rbind, .options.future = list(seed = TRUE)) %dofuture% {
+    # Inner loop over permutations
+    foreach(i = 2:howMany, .combine = rbind, .options.future = list(seed = TRUE)) %dofuture% {
       grupo <- c(permutations[[i]])
       metrics <- evaluar_modelo(grupo, train, test)
       
-      perm_progress(sprintf("Permutation %d/%d for case %d", i-1, length(permutations)-1, case))  # Update permutation progress
+      # Update the progress bars
+      case_progress(sprintf("Permutation %d/%d for Case %d", i - 1, howMany - 1, case))
+      global_progress()
       
-      
+      # Return the result as a data frame
       data.frame(Grupo = toString(grupo),
                  lr_accuracy = metrics[1],
                  lr_sensitivity = metrics[2],
@@ -191,4 +195,8 @@ with_progress({
     }
   }
 })
+
+fwrite(final_results, "SOLAR/Classification/Models/allClassif.csv")
+
+# DO PULL BEFORE PUSHING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
