e382ef44c987e39cf2c9354fac8029b4163ed15b
Cruz E. Borges <cruz.borges@deusto.es>
2025-03-20 16:29:40 +0100

    calculo el test sobre todos los datos con el fforma
    tambien anado los mejores pesos
    me falta sacar los graficos de las predicciones bien y tambien los
    boxplot de resultados

--- DeustoTech GoiEner/Resultados/FFORMA/fforma.R
+++ DeustoTech GoiEner/Resultados/FFORMA/fforma.R
@@ -5,7 +5,7 @@ library(scales)
 library(stringr)
 
 registerDoFuture()
-plan(multisession)
+plan(multicore)
 
 dir.create(file.path("./", "error"), showWarnings = FALSE)
 dir.create(file.path("./", "model"), showWarnings = FALSE)
@@ -13,14 +13,16 @@ dir.create(file.path("./", "fig"),   showWarnings = FALSE)
 
 MAPE   <- c("mean_mape","rw_mape","naive_mape","simple_mape","lr_mape","ann_mape","svm_mape","arima_mape","ses_mape","ens_mape")
 RMSE   <- c("mean_rmse","rw_rmse","naive_rmse","simple_rmse","lr_rmse","ann_rmse","svm_rmse","arima_rmse","ses_rmse","ens_rmse")
-BASE   <- c("mean_pred","rw_pred","naive_pred","simple_pred","lr_pred","ann_pred","svm_pred","arima_pred","ses_pred","ens_pred")
+BASE   <- c("mean_base","rw_base","naive_base","simple_base","lr_base","ann_base","svm_base","arima_base","ses_base","ens_base")
 PRED   <- c("dia","hora","p","zip_code","cnae") # "contracted_tariff"
 ERRORS <- c(MAPE, RMSE)
 
-df <- fread("NUEVOS DATOS/DATOS ERROR NUEVO/allMetadata_errorAne.csv")
-df <- fread("NUEVOS DATOS/DATOS ERROR NUEVO/allMetadataDEF.csv") #carpeta donde lo guarda ane
+df <- fread("allMetadata_errorAne.csv")
+#df <- fread("allMetadataDEF.csv") #carpeta donde lo guarda ane
 
 ## clean data
+names(df) <- stringr::str_replace(names(df),"pred","base")
+
 df[sapply(df, is.infinite)] <- NA
 df[is.na(cnae),     cnae:=9]
 df[is.na(zip_code), zip_code:=0]
@@ -46,10 +48,13 @@ mmape <- max(df[,..MAPE],na.rm=T)
 for (e in MAPE) df[,(e):=get(e)/mmape]
 
 ## Model
-SAMPLE <- "100000"
-SAMPLE <- "TODO"
-train  <- df[,c(..PRED,..ERRORS)]
-test   <- df[,c(..PRED,..BASE,"real")]
+SAMPLE <- "1000"
+train  <- df[,c(..PRED,..ERRORS)][sample(.N,as.numeric(SAMPLE))]
+test   <- df[,c(..PRED,..BASE,"real")] #[sample(.N,2*as.numeric(SAMPLE))]
+
+med_base_mape_weight <- 1/matrixStats::colMedians(as.matrix(df[,..MAPE]),na.rm=T)
+med_base_rmse_weight <- 1/matrixStats::colMedians(as.matrix(df[,..RMSE]),na.rm=T)
+
 rm(df)
 
 pdf(paste("fig/",SAMPLE,"-train-scaterplots.pdf"))
@@ -74,24 +79,29 @@ for (e in ERRORS)
   write.csv(n$results,row.names=F,file=paste("error/",SAMPLE,"-",e,".csv",sep=""))
   plot(n$finalModel@ymatrix,n$finalModel@fitted,main=e)
 
-  saveRDS(n, paste("model/",SAMPLE,"-",e,".rds",sep="")) ##my_model <- readRDS("FILE.rds")
+  saveRDS(n, paste("model/",SAMPLE,"-",e,".rds",sep=""))
 }
 dev.off()
 plan(sequential)
 
-pm <- pr <- data.table(matrix(ncol = length(BASE), nrow = as.numeric(SAMPLE)))
-colnames(pm) <- colnames(pr) <- BASE
 for (e in BASE)
 {
-  mm <- readRDS(paste("model/",SAMPLE,"-",str_replace(e,"pred","mape"),".rds",sep=""))
-  mr <- readRDS(paste("model/",SAMPLE,"-",str_replace(e,"pred","rmse"),".rds",sep=""))
-  pm[,(e):=as.data.table(predict(mm, newdata = test))]
-  pr[,(e):=as.data.table(predict(mr, newdata = test))]
+  mm <- readRDS(paste("model/",SAMPLE,"-",stringr::str_replace(e,"base","mape"),".rds",sep=""))
+  mr <- readRDS(paste("model/",SAMPLE,"-",stringr::str_replace(e,"base","rmse"),".rds",sep=""))
+
+  test[,(stringr::str_replace(e,"base","fforma_svn_mape_weight")):=1/predict(mm, newdata = test)]
+  test[,(stringr::str_replace(e,"base","fforma_svn_rmse_weight")):=1/predict(mr, newdata = test)]
 }
-fwrite(pm,"prediction-mape.csv")
-fwrite(pr,"prediction-rmse.csv")
-test[, fforma_svm_mape := rowSums(test[, ..BASE] * pm) / rowSums(pm)]
-test[, fforma_svm_rmse := rowSums(test[, ..BASE] * pr) / rowSums(pr)]
+
+FFORMA_WM <- stringr::str_replace(BASE,"base","fforma_svn_mape_weight")
+FFORMA_WR <- stringr::str_replace(BASE,"base","fforma_svn_rmse_weight")
+
+test[, fforma_svm_mape := rowSums(test[, ..BASE] * test[,..FFORMA_WM],na.rm=T) / rowSums(test[,..FFORMA_WM],na.rm=T)]
+test[, fforma_svm_rmse := rowSums(test[, ..BASE] * test[,..FFORMA_WR],na.rm=T) / rowSums(test[,..FFORMA_WR],na.rm=T)]
+test[, fforma_min_mape := rowSums(test[, ..BASE] * med_base_mape_weight,na.rm=T) / sum(med_base_mape_weight,na.rm=T)]
+test[, fforma_min_rmse := rowSums(test[, ..BASE] * med_base_rmse_weight,na.rm=T) / sum(med_base_rmse_weight,na.rm=T)]
+
+fwrite(test,"prediction.csv")
 
 z <- test$real != 0
 cat("RMSE FFORMA SVM MAPE","RMSE FFORMA SVM RMSE", "MAPE FFORMA SVM MAPE","MAPE FFORMA SVM RMSE","\n",  
@@ -106,3 +116,6 @@ pdf(paste("fig/",SAMPLE,"-test-scaterplots.pdf"))
   plot(test$real,test$fforma_svm_mape,main="MAPE errors")
   plot(test$real,test$fforma_svm_rmse,main="RMSE errors")
 dev.off()
+
+
+
