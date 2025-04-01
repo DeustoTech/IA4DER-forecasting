dde353cf041a540ca85532635e4e1dc803d51417
Cruz E. Borges <cruz.borges@deusto.es>
2025-04-01 07:54:59 +0200

    modificoel fforma para que funcione en casa

--- PFG/Scripts/FFORMA_errorNuevo/FForma_modeloNuevo.R
+++ PFG/Scripts/FFORMA_errorNuevo/FForma_modeloNuevo.R
@@ -1,22 +1,21 @@
-library(foreach)
-library(doParallel)
+#library(foreach)
+#library(doParallel)
 
-to_install <- c("ggplot2", "lattice", "caret", "fpp3", "class",
-                "forecast", "Metrics", "fable", 
-                "data.table", "xts", "future", "foreach", "doParallel", "RSNNS", "TTR", 
-                "quantmod", "car", "e1071", "nnet", "tools", "doFuture", "neuralnet", "gbm", 
-                "randomForest", "mice", "mltools", "zoo", "mlr3", "mlr3learners", "mlr3tuning", "mlr3verse",
-                "paradox", "xgboost", "kknn", "scales") 
+# to_install <- c("ggplot2", "lattice", "caret", "fpp3", "class",
+#                 "forecast", "Metrics", "fable", 
+#                 "data.table", "xts", "future", "foreach", "doParallel", "RSNNS", "TTR", 
+#                 "quantmod", "car", "e1071", "nnet", "tools", "doFuture", "neuralnet", "gbm", 
+#                 "randomForest", "mice", "mltools", "zoo", "mlr3", "mlr3learners", "mlr3tuning", "mlr3verse",
+#                 "paradox", "xgboost", "kknn", "scales") 
+# 
+# installed <- rownames(installed.packages())
+# for (lib in to_install) {
+#   if (!(lib %in% installed)) install.packages(lib, dependencies = TRUE)
+#   library(lib, character.only = TRUE)
+# }
 
-installed <- rownames(installed.packages())
-for (lib in to_install) {
-  if (!(lib %in% installed)) install.packages(lib, dependencies = TRUE)
-  library(lib, character.only = TRUE)
-}
-
-devtools::install_github("pmontman/customxgboost", upgrade = "never")
-devtools::install_github("pmontman/fforma", upgrade = "never")
-
+# devtools::install_github("pmontman/customxgboost", upgrade = "never")
+# devtools::install_github("pmontman/fforma", upgrade = "never")
 
 library(fforma)
 library(readr)
@@ -24,29 +23,33 @@ library(dplyr)
 library(purrr)
 library(future)
 
-
-input_folder <- "NUEVOS DATOS/OriginalData/prueba/"
-output_folder <- "NuevosResultados/FFORMA/predicciones_fforma_prueba"
-dir.create(output_folder, showWarnings = FALSE)
+input_folder <- "goi4_pst"
+output_folder <- "out"
+save_folder <- "tmp"
+dir.create(output_folder,showWarnings = FALSE, recursive = TRUE)
+dir.create(save_folder,  showWarnings = FALSE, recursive = TRUE)
 chunk_size <- 10
-save_folder <- "NuevosResultados/FFORMA/temp_fforma"
 
 #plan(multisession)
-leer_csv_como_serie <- function(filepath, h = 24) {
+plan(multicore)
+
+leer_csv_como_seriqe <- function(filepath, h = 24) {
   df <- read_csv(filepath, col_types = cols())
   kWh <- df$kWh
   ts_data <- ts(kWh, frequency = 24) 
   list(
-    x = head(ts_data, length(ts_data) - h),
-    xx = tail(ts_data, h),
+    x = tail(head(ts_data, length(ts_data) - h), 21 * h),
+    xx = tail(ts_data, 7 * h),
     h = h
   )
 }
 
 archivos <- list.files(input_folder, full.names = TRUE)
 ids_series <- tools::file_path_sans_ext(basename(archivos))
-ts_dataset <- map(archivos, leer_csv_como_serie)
+ts_dataset <- furrr:future_map(archivos, leer_csv_como_serie)
 names(ts_dataset) <- ids_series
+
+saveRDS(ts_dataset, file = "data.rds")
 
 options(future.globals.maxSize = 2 * 1024^3) #para que no de error
 
