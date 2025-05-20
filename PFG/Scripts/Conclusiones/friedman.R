da4b60ec2504b5ebe0e6ce73966fce1c26f9d017
anesanjuan <ane.sanjuan@opendeusto.es>
2025-05-20 16:59:41 +0200

    mas graficos

--- PFG/Scripts/Conclusiones/friedman.R
+++ PFG/Scripts/Conclusiones/friedman.R
@@ -1,6 +1,23 @@
 library(tidyverse)
 
-d <- data.table::fread("preds_MAPE_RMSE.csv")
+library(foreach)
+library(doParallel)
+
+to_install <- c("ggplot2", "lattice", "caret", "fpp3", "class",
+                "forecast", "Metrics", "fable", 
+                "data.table", "xts", "future", "foreach", "doParallel", "RSNNS", "TTR", 
+                "quantmod", "car", "e1071", "nnet", "tools", "doFuture", "neuralnet", "gbm", 
+                "randomForest", "mice", "mltools", "zoo", "mlr3", "mlr3learners", "mlr3tuning", "mlr3verse",
+                "paradox", "xgboost", "kknn") 
+
+installed <- rownames(installed.packages())
+for (lib in to_install) {
+  if (!(lib %in% installed)) install.packages(lib, dependencies = TRUE)
+  library(lib, character.only = TRUE)
+}
+
+# PARA MODELOS BASE #
+d <- data.table::fread("NUEVOS DATOS/DATOS ERROR NUEVO/preds_MAPE_RMSE.csv")
 
 d <- d %>% select(ends_with("mape"))
 d <- d[is.finite(rowSums(d)),]
@@ -14,6 +31,80 @@ colnames(d) <- n
 
 d <- d[,order(robustbase::colMedians(d))]
 b <- boxplot(d,outline=F)
+
+friedman_result <- friedman.test(d)
+print(friedman_result)
+
+f <- PMCMRplus::frdAllPairsNemenyiTest(d)
+
+p <- rbind(1,f$p.value)
+p <- cbind(p, 1)
+diag(p) <- 1
+p <- as.matrix(Matrix::forceSymmetric(p, "L"))
+rownames(p)[dim(f$p.value)+1] <- rownames(f$p.value)[dim(f$p.value)[1]]
+colnames(p)[dim(f$p.value)+1] <- rownames(f$p.value)[dim(f$p.value)[1]]
+
+l <- multcompView::multcompLetters(p)
+
+text(
+  x=c(1:length(colnames(d))),
+  y=b$stats[nrow(b$stats),] + 10,
+  as.character(print(l))
+)
+
+
+# PARA MODELOS BASE + ENSEMBLE SIMPLE #
+d <- data.table::fread("NUEVOS DATOS/DATOS ERROR NUEVO/preds_MAPE_RMSE.csv")
+
+d <- d %>% select(ends_with("mape"))
+d <- d[is.finite(rowSums(d)),]
+
+n <- stringr::str_split_fixed(names(d),pattern="_",n=2)[,1]
+
+d <- as.matrix(d)
+rownames(d) <- 1:nrow(d)
+colnames(d) <- n
+
+d <- d[,order(robustbase::colMedians(d))]
+b <- boxplot(d,outline=F)
+
+friedman_result <- friedman.test(d)
+print(friedman_result)
+
+f <- PMCMRplus::frdAllPairsNemenyiTest(d)
+
+p <- rbind(1,f$p.value)
+p <- cbind(p, 1)
+diag(p) <- 1
+p <- as.matrix(Matrix::forceSymmetric(p, "L"))
+rownames(p)[dim(f$p.value)+1] <- rownames(f$p.value)[dim(f$p.value)[1]]
+colnames(p)[dim(f$p.value)+1] <- rownames(f$p.value)[dim(f$p.value)[1]]
+
+l <- multcompView::multcompLetters(p)
+
+text(
+  x=c(1:length(colnames(d))),
+  y=b$stats[nrow(b$stats),] + 10,
+  as.character(print(l))
+)
+
+
+###### MODELOS FFORMA #######
+datosMAPE <- fread("NuevosResultados/PrediccionErrorNuevo/PrediccionMAPE/REDUCIDO_DIA/FFORMA_MAPE.csv")
+d <- datosMAPE %>% select(matches("^FFORMA_.*_MAPE$"))
+d <- d[is.finite(rowSums(d)),]
+
+n <- gsub("_MAPE$", "", names(d))
+
+d <- as.matrix(d)
+rownames(d) <- 1:nrow(d)
+colnames(d) <- n
+
+d <- d[,order(robustbase::colMedians(d))]
+b <- boxplot(d,outline=F)
+
+friedman_result <- friedman.test(d)
+print(friedman_result)
 
 f <- PMCMRplus::frdAllPairsNemenyiTest(d)
 
--- PFG/Scripts/Graficos/MEMORIA/GraficosPredError.R
+++ PFG/Scripts/Graficos/MEMORIA/GraficosPredError.R
@@ -252,3 +252,46 @@ filtrar_y_resumir_opt <- function(df) {
 summary_dia <- filtrar_y_resumir_opt(datosCombinadosDIA)
 summary_hora <- filtrar_y_resumir_opt(combinedPredsHORA)
 summary_diahora <- filtrar_y_resumir_opt(combinedPredsDIAyHORA)
+
+
+###### BOXPLOT SIMPLE CON LOS DATOS REDUCIDOS POR DIA ##########
+feats <- fread("NUEVOS DATOS/DATOS ERROR NUEVO/preds_MAPE_RMSE.csv")
+feats$ID <- feats$id
+feats <- feats %>% select(-id)
+feats <- feats %>% select(ID, dia, hora, real, mean_pred, rw_pred, naive_pred, simple_pred, lr_pred, ann_pred, svm_pred, arima_pred, ses_pred, ens_pred, mean_mape, rw_mape, naive_mape, simple_mape, lr_mape, ann_mape, svm_mape, arima_mape, ses_mape, ens_mape)
+feats$Real <- feats$real
+feats <- feats %>% select(-real)
+
+combinedPredsDIA <- fread("NuevosResultados/PrediccionErrorNuevo/PrediccionMAPE/REDUCIDO_DIA/combinedPreds.csv")
+combinedPredsDIA <- combinedPredsDIA %>% select(ID, starts_with("Real_"), starts_with("Predicted_"), starts_with("MAPE_"))
+combinedPredsDIA <- combinedPredsDIA %>% distinct(ID, .keep_all = TRUE)
+
+datosCombinadosDIA <- feats %>%
+  left_join(combinedPredsDIA, by = "ID")
+setDT(datosCombinadosDIA)
+
+mape_lm <- grep("MAPE_.*_lm$", names(datosCombinadosDIA), value = TRUE)
+mape_rf <- grep("MAPE_.*_rf$", names(datosCombinadosDIA), value = TRUE)
+mape_knn <- grep("MAPE_.*_knn$", names(datosCombinadosDIA), value = TRUE)
+mape_xgb <- grep("MAPE_.*_xgboost$", names(datosCombinadosDIA), value = TRUE)
+
+graficar_boxplot <- function(columnas) {
+  data <- datosCombinadosDIA[, ..columnas]
+  matrix_data <- as.matrix(data)
+  matrix_data <- matrix_data[complete.cases(matrix_data) & rowSums(is.finite(matrix_data)) == length(columnas), ]
+  par(mar = c(10, 4, 4, 2))
+  b <- boxplot(matrix_data, outline = FALSE, las = 3, ylab = "MAPE")
+  medianas <- apply(matrix_data, 2, median, na.rm = TRUE)
+  text(
+    x = 1:length(medianas),
+    y = medianas + 7,  # Posición justo encima del upper whisker
+    labels = round(medianas, 1),  # Redondear si lo deseas
+    cex = 1.5
+  )
+}
+
+graficar_boxplot(mape_lm)
+graficar_boxplot(mape_rf)
+graficar_boxplot(mape_knn)
+graficar_boxplot(mape_xgb)
+
